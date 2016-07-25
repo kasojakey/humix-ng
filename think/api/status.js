4fc8f7e1e70e20ef5d62a821343a24498d6b4498
Jimmy Liao <sjliao@tw.ibm.com>
2016-07-25 15:12:57 +0800

    commit for 1. change default bluemix service binding, 2. reduce verbose logging message, 3. temp fix for grant libraries

--- think/api/status.js
+++ think/api/status.js
@@ -13,34 +13,31 @@ module.exports = {
 
     getSenseStatus: function(req, res) {
 
-        log.info('getSenseStatus');
 
         var senseId = req.params.senseId;
         var status = sense.getSenseStatus(senseId);
-        log.info('sense status: ' + JSON.stringify(status));
+        log.debug('sense status: ' + JSON.stringify(status));
 
         res.send(status);        
     },
 
     getAllModuleStatus: function (req, res) { 
 
-        log.info('getAllModuleStatus');        
 
         var senseId = req.params.senseId;    
         var status = sense.getAllModuleStatus(senseId);
-        log.info('all module status: ' + JSON.stringify(status));
+        log.debug('all module status: ' + JSON.stringify(status));
 
         res.send(status);
     },
 
      getModuleStatus: function (req, res) { 
 
-        log.info('getModuleStatus');        
 
         var senseId =  req.params.senseId;
         var moduleId = req.params.moduleId;
         var status = sense.getModuleStatus(senseId, moduleId);
-        log.info('module status: ' + JSON.stringify(status));
+        log.debug('module status: ' + JSON.stringify(status));
 
         res.send(status);               
     },
